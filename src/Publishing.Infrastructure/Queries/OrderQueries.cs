8725a3059d1e73b8ddf300650822ccc701297baf
MaxK789 <maksym.kapitulin@nure.ua>
2025-06-27 14:25:59 +0300

    fix active orders query

--- src/Publishing.Infrastructure/Queries/OrderQueries.cs
+++ src/Publishing.Infrastructure/Queries/OrderQueries.cs
@@ -14,7 +14,7 @@ namespace Publishing.Infrastructure.Queries
 
         public Task<DataTable> GetActiveAsync()
         {
-            const string sql = @"SELECT O.namePrintery, Prod.typeProduct, Prod.nameProduct, Per.FName, Per.LName, O.dateOrder, O.dateStart, O.dateFinish, O.statusOrder, O.price FROM(Orders O INNER JOIN Product Prod ON Prod.idProduct = O.idProduct ) INNER JOIN Person Per ON Per.idPerson = Prod.idPerson WHERE O.statusOrder = 'в роботі' ORDER BY O.dateOrder";
+            const string sql = @"SELECT O.namePrintery, Prod.typeProduct, Prod.nameProduct, Per.FName, Per.LName, O.dateOrder, O.dateStart, O.dateFinish, O.statusOrder, O.price FROM(Orders O INNER JOIN Product Prod ON Prod.idProduct = O.idProduct ) INNER JOIN Person Per ON Per.idPerson = Prod.idPerson WHERE O.statusOrder IN ('в роботі', 'InProgress') ORDER BY O.dateOrder";
             return _helper.QueryDataTableAsync(sql);
         }
 
